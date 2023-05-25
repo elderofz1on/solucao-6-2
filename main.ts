2c493e7eb67ac467f33678b718f159a3f8c524b9
Wagner <wprodriguesbr@gmail.com>
2023-05-25 10:29:44 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
+basic.forever(function () {
+	
+})
